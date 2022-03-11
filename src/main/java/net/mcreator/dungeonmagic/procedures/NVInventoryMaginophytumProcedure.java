a3feaad60010449c57342c9268ae3668b589d4da
serjoss95 <bonnardmae@gmail.com>
2022-03-11 17:29:25 +0100

    fixing stuff i guess

--- src/main/java/net/mcreator/dungeonmagic/procedures/NVInventoryMaginophytumProcedure.java
+++ src/main/java/net/mcreator/dungeonmagic/procedures/NVInventoryMaginophytumProcedure.java
@@ -1,6 +1,16 @@
 package net.mcreator.dungeonmagic.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.dungeonmagic.block.MaginophytumBlock;
+import net.mcreator.dungeonmagic.DungeonmagicMod;
+
+import java.util.Map;
 
 public class NVInventoryMaginophytumProcedure {
 
@@ -10,13 +20,10 @@ public class NVInventoryMaginophytumProcedure {
 				DungeonmagicMod.LOGGER.warn("Failed to load dependency entity for procedure NVInventoryMaginophytum!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(MaginophytumBlock.block)) : false) {
 			if (entity instanceof LivingEntity)
 				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, (int) 100000, (int) 1));
 		}
 	}
-
 }
